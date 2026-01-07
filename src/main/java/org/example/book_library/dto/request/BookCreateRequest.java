2676e5b7fa2d986720ca73385711782062cd9c44
TaurineManiac <ignatladno@gmail.com>
2026-01-07 23:02:54 +0300

    change from everytime updating db to migration DDL

--- src/main/java/org/example/book_library/dto/request/BookCreateRequest.java
+++ src/main/java/org/example/book_library/dto/request/BookCreateRequest.java
@@ -3,7 +3,6 @@ package org.example.book_library.dto.request;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 import java.time.LocalDate;
