e14a89f9a360b3ff5e8b367fbdb6464c17e2abd1
TaurineManiac <ignatladno@gmail.com>
2026-01-23 16:24:43 +0300

    created test controller

--- src/test/java/org/example/book_library/controller/BookControllerTest.java
+++ src/test/java/org/example/book_library/controller/BookControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
