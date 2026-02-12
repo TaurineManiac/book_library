6677811833befd49fa80e4dad589ae21da03fe84
TaurineManiac <ignatladno@gmail.com>
2026-02-12 11:08:07 +0300

    created authentication config, it will be useful in AuthService and AuthFilter

--- src/main/java/org/example/book_library/security/AuthenticationConfig.java
+++ src/main/java/org/example/book_library/security/AuthenticationConfig.java
@@ -1,4 +1,44 @@
 package org.example.book_library.security;
 
+import lombok.RequiredArgsConstructor;
+import org.example.book_library.repository.inter.UserRepositoryInter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
 public class AuthenticationConfig {
+
+    private final UserRepositoryInter userRepositoryInter;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService () {
+        return username -> userRepositoryInter.findByUsername(username)
+                .orElseThrow(() ->new UsernameNotFoundException("User not found."));
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
