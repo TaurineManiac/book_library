fc3b9ab4b57723aea861cb2e7c180a5879c5d278
TaurineManiac <ignatladno@gmail.com>
2026-02-15 16:14:31 +0300

    added annotation for succesfully transefer Page into json

--- src/main/java/org/example/book_library/BookLibraryApplication.java
+++ src/main/java/org/example/book_library/BookLibraryApplication.java
@@ -2,8 +2,10 @@ package org.example.book_library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class BookLibraryApplication {
 
     public static void main(String[] args) {
