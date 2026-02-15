45e795eddcd470443fcd292b2e8afd957ed60fe4
TaurineManiac <ignatladno@gmail.com>
2026-02-15 11:52:28 +0300

    add login method

--- src/main/java/org/example/book_library/service/imple/AuthServiceImple.java
+++ src/main/java/org/example/book_library/service/imple/AuthServiceImple.java
@@ -7,15 +7,23 @@ import org.example.book_library.dto.request.UserRegistrationRequest;
 import org.example.book_library.dto.response.UserRegistrationResponse;
 import org.example.book_library.exception.custom.UserAlreadyExistsException;
 import org.example.book_library.repository.inter.UserRepositoryInter;
+import org.example.book_library.security.JWTService;
 import org.example.book_library.service.inter.AuthService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImple implements AuthService {
     private final UserRepositoryInter userRepositoryInter;
     private final PasswordEncoder passwordEncoder;
+    private final JWTService jwtService;
+    private final AuthenticationManager authenticationManager;
 
 
     @Override
@@ -37,5 +45,21 @@ public class AuthServiceImple implements AuthService {
                 .success(true)
                 .message("User registered successfully!")
                 .build();
+    }
+
+    @Override
+    public String login(String username, String password) {
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+        User user = userRepositoryInter.findByUsername(username).orElseThrow();
+
+        Map<String,Object> map = new HashMap<>();
+        map.put("role",user.getRole().name());
+        map.put("id", user.getId());
+
+        return jwtService.generateJWTToken(map,user);
+
+
     }
 }
